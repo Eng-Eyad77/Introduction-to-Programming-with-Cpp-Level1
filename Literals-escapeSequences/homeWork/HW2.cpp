0202ded2274e23694c582664941047c43e6aefca
Eyad Maccawy <s444003279@uqu.edu.sa>
2026-04-16 12:19:10 +0300

    Refactor code structure for improved readability and maintainability

---
+++ Literals-escapeSequences/homeWork/HW2.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    cout<< "Dear Sir\\Madam, \n";
+
+    cout<< "How are you? \n";
+
+    cout<< "My name is \"Eyad\", nice to meet you. \n";
+
+    return 0;
+}
---
+++ catching-up/HW-1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   cout << "******************************\n";
+   cout << "Name: Eyad Adnan Maccawy.\n";
+   cout << "Age: 21 years.\n";
+   cout << "City: Makkah\n";
+   cout << "Country: Saudi Arabia\n";
+   cout << "******************************\n";
+}
