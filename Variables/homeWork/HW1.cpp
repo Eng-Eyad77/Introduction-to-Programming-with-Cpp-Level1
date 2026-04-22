e91e7bb791b71fd17c5691a379b5c2d6aafa3df2
Eyad Maccawy <s444003279@uqu.edu.sa>
2026-04-22 15:34:07 +0300

    user input done

--- Variables/homeWork/HW1.cpp
+++ Variables/homeWork/HW1.cpp
@@ -2,23 +2,44 @@
 using namespace std;
 
 int main(){
-    string name = "Eyad Adnan Makkawi.";
-    string age = "21 Years.";
-    string city = "Makkah.";
-    string country = "Saudi Arabia.";
-    int monthlySalary = 5000;
-    int yearlySalary = 60000;
-    char gender = 'M';
-    char married = '1'; 
+    string name;
+    int age;
+    string city;
+    string country;
+    float monthlySalary;
+    char gender;
+    bool isMarried; 
+
+    cout << "Please Enter Your Full Name: " << endl;
+    cin >> name;
+    cout << "Please Enter Your Age: " << endl;
+    cin >> age;
+    cout << "Please Enter Your City: " << endl;
+    cin >> city;
+    cout << "Please Enter Your Country: " << endl;
+    cin >> country;
+    cout << "Please Enter Your Salary Peer Month: " << endl;
+    cin >> monthlySalary;
+    cout << "Please Enter Your Gender M OR F: " << endl;
+    cin >> gender;
+    cout << "Are you Married 1-for yes 0-for no: " << endl;
+    cin >> isMarried;
+
+
+
+
+
+
+
     cout << "******************************************** \n";
     cout << "Name: " << name <<endl;
-    cout << "Age: "<< age << endl;
+    cout << "Age: "<< age <<" Years." <<  endl;
     cout << "City: "<< city << endl;
     cout << "Country: "<< country << endl;
     cout << "Monthly Salary: "<< monthlySalary << endl;
-    cout << "Yearly Salary: "<< yearlySalary << endl;
+    cout << "Yearly Salary: "<< monthlySalary * 12 << endl;
     cout << "Gender: "<< gender << endl;
-    cout << "Married: "<< married << endl;
+    cout << "Married: "<< isMarried << endl;
     cout << "******************************************** \n";
 
 
