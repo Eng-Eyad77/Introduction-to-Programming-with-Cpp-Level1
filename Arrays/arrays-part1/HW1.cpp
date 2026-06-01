eb9f9a581cb4d1b6b90ee52fc4ede9f1be28115b
Eyad Maccawy <s444003279@uqu.edu.sa>
2026-06-01 05:33:34 +0300

    done from arrays part1

---
+++ Arrays/arrays-part1/HW1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+    float gradeArray[3];
+    cout << "Please enter grade1: " << endl;
+    cin >> gradeArray[0];
+
+    cout << "Please enter grade2: " << endl;
+    cin >> gradeArray[1];
+
+    cout << "Please enter grade3: " << endl;
+    cin >> gradeArray[2];
+
+    float avgGrade = (gradeArray[0] +gradeArray[1] + gradeArray[2]) / 3;
+
+    cout << "*************************************************" << endl;
+    cout << "The average of grades is: " << avgGrade << endl;
+    
+
+    return 0;
+}
